1bc64013cabcec3cd391c52bb96e9c1d87872c39
kei0101xw <kei0101xw@icloud.com>
2025-05-09 15:21:18 +0900

    [refactor] Homeのルーティングを追加した

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Test from "./pages/Test";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
           <Route path="/test" element={<Test />} />
         </Routes>
       </div>
