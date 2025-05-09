d75858b8181a5cd4e415e560a2766f6ccbf529d9
ToFuruta <tomohiro.9.10@icloud.com>
2025-05-09 17:14:18 +0900

    [style] Startファイルの修正

--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import logo from "./assets/logo.png"; // 相対パスでインポート
+import logo from "../assets/logo.png"; // 相対パスでインポート
+import "./Start.css";
 
 const Start = () => {
   return (
     <div>
       <h1>シジャン博多一番街店</h1>
       <img src={logo} alt="ロゴ画像" className="logo" />
-      <h2>ログイン</h2>
-      <button>従業員の方はこちら</button>
-      <button>管理者の方はこちら</button>
+      <div class="login-box">
+        <h2 class="login-title">ログイン</h2>
+        <button class="login-button employee">従業員の方はこちら</button>
+        <button class="login-button admin">管理者の方はこちら</button>
+      </div>
     </div>
   );
 };
