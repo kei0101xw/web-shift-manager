5397708f8081022cbbb6260e1ce563766462e5b5
ToFuruta <tomohiro.9.10@icloud.com>
2025-05-09 16:04:46 +0900

    [style] Routeのコードの修正

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Test from "./pages/Test";
 import Home from "./pages/Home";
+import LoginEmployee from "./pages/LoginEmployee";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/loginemployee" element={<LoginEmployee />} />
           <Route path="/test" element={<Test />} />
         </Routes>
       </div>
--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -7,8 +7,8 @@ const Start = () => {
       <h1>シジャン博多一番街店</h1>
       <img src={logo} alt="ロゴ画像" className="logo" />
       <h2>ログイン</h2>
-      <p>従業員の方はこちら</p>
-      <p>管理者の方はこちら</p>
+      <button>従業員の方はこちら</button>
+      <button>管理者の方はこちら</button>
     </div>
   );
 };
