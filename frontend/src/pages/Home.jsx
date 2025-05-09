3e4b74a60b5727956e510e2fff73b4b1b5322795
kei0101xw <kei0101xw@icloud.com>
2025-05-09 17:47:34 +0900

    [change] ホームページの基本的なUIを実装

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import "./Home.css";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome back, Tomohiro Furuta</h1>
-      <button>全体のシフト</button>
-      <button>Myシフト</button>
-      <button>本日のシフト</button>
-
-      <button>シフト申請・修正</button>
-
-      <button>給料計算</button>
-    </div>
+    <>
+      <Header />
+      <div className="home-container">
+        <div>Welcome back, Tomohiro Furuta</div>
+        <div className="all-button-group">
+          <div className="shift-button-group">
+            <button className="shift-button">全体のシフト</button>
+            <button className="shift-button">Myシフト</button>
+            <button className="shift-button">本日のシフト</button>
+          </div>
+          <hr></hr>
+          <div className="shift-apply-button-group">
+            <button className="shift-apply--button">シフト申請・修正</button>
+          </div>
+          <hr></hr>
+          <div className="salary-calc-button-group">
+            <button className="salary-calc-button">給料計算</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
