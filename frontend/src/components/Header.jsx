21a657072110ab2bde7fb84a7f26fad3ab78bdd5
ToFuruta <tomohiro.9.10@icloud.com>
2025-05-09 14:33:35 +0900

    [feat] Headerファイルにハンバーガボタンを追加

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,22 @@
-import React from "react";
-import "./Header.css"; // CSSファイルをインポート
+import React, { useState } from "react";
+import "./Header.css";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
-      <h1 className="logo">My Website</h1>
-      <nav>
+      <h1 className="logo">シジャン博多一番街店</h1>
+      <button className="hamburger" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+      <nav className={`nav ${menuOpen ? "open" : ""}`}>
         <ul className="nav-list">
           <li>
             <a href="/" className="nav-item">
