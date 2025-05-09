7e5af11efa4fdec8e78517444fe53f2a4cc59a1a
kei0101xw <kei0101xw@icloud.com>
2025-05-09 23:44:07 +0900

    [change] ホームのUIを整えた

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,74 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import Header from "../components/Header";
+import calendar from "../assets/calendar.png";
+import human from "../assets/human.png";
+import people from "../assets/people.png";
+import money from "../assets/money.png";
+import paper from "../assets/paper.png";
+import eraser from "../assets/eraser.png";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Header />
       <div className="home-container">
         <div>Welcome back, Tomohiro Furuta</div>
         <div className="all-button-group">
+          <div className="button-title shift">シフトの確認</div>
           <div className="shift-button-group">
-            <button className="shift-button">全体のシフト</button>
-            <button className="shift-button">Myシフト</button>
-            <button className="shift-button">本日のシフト</button>
+            <button
+              className="shift-button all"
+              onClick={() => navigate("/allshift")}
+            >
+              <div className="shift-button-sentence">全体のシフト</div>
+              <img src={people} alt="ロゴ画像" className="people-logo" />
+            </button>
+            <button
+              className="shift-button my"
+              onClick={() => navigate("/myshift")}
+            >
+              <div className="shift-button-sentence">Myシフト</div>
+              <img src={human} alt="ロゴ画像" className="human-logo" />
+            </button>
+            <button
+              className="shift-button today"
+              onClick={() => navigate("/todayshift")}
+            >
+              <div className="shift-button-sentence">本日のシフト</div>
+              <img src={calendar} alt="ロゴ画像" className="calendar-logo" />
+            </button>
           </div>
           <hr></hr>
-          <div className="shift-apply-button-group">
-            <button className="shift-apply--button">シフト申請・修正</button>
+          <div className="button-title submit">シフトの提出・修正</div>
+          <div className="shift-apply-modify-group">
+            <button
+              className="shift-apply-button"
+              onClick={() => navigate("/shiftapply")}
+            >
+              <div className="shift-apply-button-sentence">シフト申請</div>
+              <img src={paper} alt="ロゴ画像" className="paper-logo" />
+            </button>
+            <button
+              className="shift-modify-button"
+              onClick={() => navigate("/shiftedit")}
+            >
+              <div className="shift-modify-button-sentence">シフト修正</div>
+              <img src={eraser} alt="ロゴ画像" className="eraser-logo" />
+            </button>
           </div>
           <hr></hr>
+          <div className="button-title shift">給料</div>
           <div className="salary-calc-button-group">
-            <button className="salary-calc-button">給料計算</button>
+            <button
+              className="salary-calc-button"
+              onClick={() => navigate("/salarycalc")}
+            >
+              <div className="salary-calc-sentence">給料計算</div>
+              <img src={money} alt="ロゴ画像" className="money-logo" />
+            </button>
           </div>
         </div>
       </div>
