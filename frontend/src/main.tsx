52e6807eba589fa521bc8f5aa2504910ae676a27
kei0101xw <kei0101xw@icloud.com>
2025-07-26 23:59:35 +0900

    [change] main.jsxの拡張子をtsxに変更

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
+// 非nullアサーション（!）で nullでないことを保証
+const rootElement = document.getElementById("root")!;
+
+createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
       <App />
