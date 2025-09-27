fa4272c8f7d05fa53db5e9d3125b6e3d4b4ae53b
ToFuruta <tomohiro.9.10@icloud.com>
2025-09-27 19:31:16 +0900

    [change]フロントとバックを疎通するために修正

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import LoginEmployee from "./pages/LoginEmployee";
