25609f9548fa8158980a18f73b6d2a20d4f923ef
ToFuruta <tomohiro.9.10@icloud.com>
2025-10-07 15:58:26 +0900

    [fix] フロントの環境修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
+    port: 5173,
     proxy: {
-      "/api": { target: "http://backend:8080", changeOrigin: true },
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
     },
   },
 });
